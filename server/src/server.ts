6578d74f1f499e06603851285ff6f9f4826e5c74
science <science@Science.sciencebit.com>
2020-10-22 01:49:46 -0300

    Atualizando o README

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,8 @@
+require("dotenv").config();
+
 import express from 'express';
 import cors from 'cors';
+
 import routes from './routes';
 
 const app = express();
@@ -8,4 +11,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT);
