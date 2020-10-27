55e3a9ab81ae34f7f0fb53c22ccc6c5d9fdc0495
science <science@Science.sciencebit.com>
2020-10-27 04:23:37 -0300

    adiciona procfile

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -7,17 +7,6 @@ const routes = express.Router();
 const classesController = new ClassesController();
 const connectionsController = new ConnectionsController();
 
-/**
- * GET: Buscar ou listar uma informação.
- * POST: Criar uma nova informação.
- * PUT: Atualizar uma informação existente.
- * DELETE: Deletar uma informação existente.
- * 
- * Corpo (Request Body): Dados para criação ou atualização de um registro.
- * Route Params: Identificar qual recurso eu quero atualizar ou deletar.
- * Query Params: Paginação, filtros, odernação.
- */
-
 routes.get('/classes', classesController.index);
 routes.post('/classes', classesController.create);
 
